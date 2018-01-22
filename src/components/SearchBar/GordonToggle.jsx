80dcb6951835b6d854718308754868926b4a2e1d
Luciano Oliveira <luciano101@msn.com>
2018-01-22 15:41:12 -0800

    return true/false on click of toggle

--- src/components/SearchBar/GordonToggle.jsx
+++ src/components/SearchBar/GordonToggle.jsx
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 class GordonToggle extends Component {
   constructor(props){
     super(props);
+    this.state = { on: false }
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle(){
+    this.setState({ 
+      on: !this.state.on
+    });
   }
 
   render(){
+    console.log(this.state.on);
     return(
       <div className="gordon-toggle">
-          <i className="fa fa-toggle-on" aria-hidden="true"></i>
+          <i className="fa fa-toggle-on" aria-hidden="true"
+            onClick={this.handleToggle}>
+          </i>
           <h3>Gordon ON</h3>
       </div>
     )
