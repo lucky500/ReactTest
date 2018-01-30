32266e7748b4830ca75eb66b6f1ba9e0b6cf65cc
Luciano Oliveira <luciano101@msn.com>
2018-01-29 18:10:23 -0800

    add data.json

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'
-import Logo from '../../main/img/cirrus_logo.png';
-//import './Header.less';
+
 
 export default props =>(
   <Navbar>
     <NavbarBrand>
-        <img src={Logo} alt="logo" className="logo" />
+        
     </NavbarBrand>
   </Navbar>
 )
